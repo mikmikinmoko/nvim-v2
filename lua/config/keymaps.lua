a4fda8e30df0c0765a6b70e7bb96de15abb4ac44
mikmikinmoko <mikmikinmoko05@gmail.com>
2025-07-05 23:43:46 +0800

    updated nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-d>", "5j", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-u>", "5k", { noremap = true, silent = true })
-
 -- Toggle Tab next and prev and new tab
 vim.keymap.set("n", "<leader>tt", ":tabnew<CR>", { desc = "New Tab" })
 vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { desc = "Next Tab" })
 vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { desc = "Previous Tab" })
+-- vim.keymap.set("n", "<leader>tp", ":tabprevious<CR>", { desc = "Previous Tab" })
 
 -- Toggle Buffer next and prev
 -- vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next Buffer" })
@@ -23,10 +21,10 @@ vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move Down" })
 vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move Up" })
 vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move Right" })
 
-vim.keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>", { desc = "Toggle Claude Code" })
+-- vim.keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>", { desc = "Toggle Claude Code" })
 
 -- Toggle Terminal
-vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Terminal horizontal" })
+-- vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Terminal horizontal" })
 
 -- search current buffer
 vim.keymap.set("n", "/", function()
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -24,10 +24,10 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          -- ["<Tab>"] = cmp.mapping.select_next_item(),
+          -- ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- ["<C-Space>"] = cmp.mapping.complete(),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
